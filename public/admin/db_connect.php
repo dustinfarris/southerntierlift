e13a2ef8d7777aa9388764e7d4ec6b2a0efc62a4
Dustin Farris <dustin@dustinfarris.com>
2012-07-10 20:12:24 -0400

    Copied db_connect script to admin/db_connect

--- public/admin/db_connect.php
+++ public/admin/db_connect.php
@@ -9,11 +9,21 @@
 ** 
 */
 
-$link = mysql_connect('localhost', 'stl_backend', 'm+,!(pu24ID7[)'); //Connects to the database at "localhost"
+/* New DB connect info - Dustin ****/
+
+$host = 'localhost';
+$user = 'stl_frontend';
+$pass = 'xvztktg';
+
+$link = mysql_connect($host, $user, $pass);
+
+/*********/
+
+
 if(!$link) {
     //halt execution if cannot connect
     die("Cannot connect to the database! ".mysql_error());
 }
-mysql_select_db('southerntierlift', $link); 
+mysql_select_db('southerntierlift', $link);
 
 ?>
